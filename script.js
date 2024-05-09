c9274f12483469bfca9ba64132b8436374e023cd
Joseph Bataller <batallerjem208@gmail.com>
2024-05-09 11:27:19 +0800

    responsive

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-var images = [
-    { url: 'images/elits-president.png', width: '305px' },
-    { url: 'images/elits-1st-year-rep.png', width: '310px' },
-    { url: 'images/elits-2nd-year-rep.png', width: '325px' },
-    { url: 'images/luwi.png', width: '360px' },
-    { url: 'images/lev.png', width: '500px' },
-    // Add more image URLs and widths as needed
-  ];
+// var images = [
+//     { url: 'images/elits-president.png', width: '305px' },
+//     { url: 'images/elits-1st-year-rep.png', width: '310px' },
+//     { url: 'images/elits-2nd-year-rep.png', width: '325px' },
+//     // { url: 'images/luwi.png', width: '360px' },
+//     // { url: 'images/lev.png', width: '500px' },
+//     { url: 'images/elits-secretary.png', width: '325px' },
+//     { url: 'images/elits-asst-secretary.png', width: '290px' },
+//     { url: 'images/elits-secretary.png', width: '335px' },
+//     { url: 'images/elits-auditor.png', width: '320px' },
+//     { url: 'images/elits-business-manager.png', width: '310px' },
+//     { url: 'images/elits-public-relations-officer.png', width: '340px' },
+//     { url: 'images/elits-gad-representative.png', width: '300px' },
+//     { url: 'images/elits-3rd-year-rep.png', width: '315px' },
+//     { url: 'images/elits-4th-year-rep.png', width: '300px' },
+//     // Add more image URLs and widths as needed
+//   ];
 
-  // Function to set random image for elits-member
-function setRandomImage() {
-var randomIndex = Math.floor(Math.random() * images.length);
-var randomImage = images[randomIndex];
-document.querySelector('.elits-member').src = randomImage.url;
-document.querySelector('.elits-member').style.width = randomImage.width;
-}
+//   // Function to set random image for elits-member
+// function setRandomImage() {
+// var randomIndex = Math.floor(Math.random() * images.length);
+// var randomImage = images[randomIndex];
+// document.querySelector('.elits-member').src = randomImage.url;
+// document.querySelector('.elits-member').style.width = randomImage.width;
+// }
 
-// Call the function when the page loads and reloads
-window.onload = setRandomImage;
-window.onbeforeunload = setRandomImage; // For modern browsers
-window.onunload = setRandomImage; 
+// // Call the function when the page loads and reloads
+// window.onload = setRandomImage;
+// window.onbeforeunload = setRandomImage; // For modern browsers
+// window.onunload = setRandomImage; 
